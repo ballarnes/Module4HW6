e12338950bb4c78e86aebe4f0ba72c580074df1f
Aleksey Veselovskiy <ballarnes@gmail.com>
2021-11-21 15:07:35 +0200

    add an empty project

---
+++ Module4HW6/Module4HW6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module4HW6
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
